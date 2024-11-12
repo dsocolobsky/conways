c8cb674466860b4db307829d48e68afe7d32c647
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-11-12 17:08:06 -0300

    Add tests for Grid

--- src/grid.rs
+++ src/grid.rs
@@ -2,7 +2,7 @@ pub const GRID_WIDTH: usize = 32;
 pub const GRID_HEIGHT: usize = 32;
 pub const CELL_SIZE: f32 = 30.0;
 
-#[derive(Copy, Clone, PartialEq, Default)]
+#[derive(Copy, Clone, PartialEq, Default, Debug)]
 pub enum CellState {
     #[default]
     Dead,
@@ -20,6 +20,13 @@ pub fn get_cell_state(grid: &Grid, x: usize, y: usize) -> Option<CellState> {
         Some(grid[x][y])
     } else {
         None
+    }
+}
+
+pub fn cell_is_alive(grid: &Grid, x: usize, y: usize) -> bool {
+    match get_cell_state(grid, x, y) {
+        Some(state) => state == CellState::Alive,
+        _ => false,
     }
 }
 
@@ -61,11 +68,9 @@ fn alive_neighbours(grid: &Grid, x: usize, y: usize) -> usize {
 }
 
 fn next_state_for_cell(grid: &Grid, x: usize, y: usize) -> CellState {
-    let Some(cell_state) = get_cell_state(grid, x, y) else {
-        panic!("Invalid coordinate");
-    };
+    let currently_alive = cell_is_alive(grid, x, y);
     let neighbours = alive_neighbours(grid, x, y);
-    if cell_state == CellState::Alive {
+    if currently_alive {
         if neighbours < 2 || neighbours > 3 {
             CellState::Dead
         } else {
@@ -98,4 +103,71 @@ pub fn create_initial_grid(alive: Vec<(usize, usize)>) -> Grid {
         new_grid[i][j] = CellState::Alive;
     }
     new_grid
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::grid::{cell_is_alive, create_initial_grid, next_state_for_grid, CellState};
+
+    #[test]
+    fn create_grid() {
+        let grid = create_initial_grid(vec![(5, 5)]);
+        for (i, &row) in grid.iter().enumerate() {
+            for (j, &cell) in row.iter().enumerate() {
+                if i == 5 && j == 5 {
+                    assert_eq!(cell, CellState::Alive)
+                } else {
+                    assert_eq!(cell, CellState::Dead)
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn cell_dies_of_underpopulation() {
+        let grid = create_initial_grid(vec![(5, 5)]);
+        assert!(cell_is_alive(&grid, 5, 5));
+        let grid = next_state_for_grid(&grid);
+        assert!(!cell_is_alive(&grid, 5, 5));
+    }
+
+    #[test]
+    fn cell_dies_of_overpopulation() {
+        let grid = create_initial_grid(vec![(5, 5), (5, 6), (5, 4), (4, 5), (6, 6)]);
+        assert!(cell_is_alive(&grid, 5, 5));
+        let grid = next_state_for_grid(&grid);
+        assert!(!cell_is_alive(&grid, 5, 5));
+    }
+
+    #[test]
+    fn cell_survives() {
+        let grid = create_initial_grid(vec![(5, 5), (5, 6), (5, 4)]);
+        assert!(cell_is_alive(&grid, 5, 5));
+        let grid = next_state_for_grid(&grid);
+        assert!(cell_is_alive(&grid, 5, 5));
+    }
+
+    #[test]
+    fn blinker_pattern() {
+        let grid = create_initial_grid(vec![(5, 5), (5, 6), (5, 7)]);
+        assert!(cell_is_alive(&grid, 5, 5));
+        assert!(cell_is_alive(&grid, 5, 6));
+        assert!(cell_is_alive(&grid, 5, 7));
+
+        let grid = next_state_for_grid(&grid);
+        assert!(cell_is_alive(&grid, 4, 6));
+        assert!(cell_is_alive(&grid, 5, 6));
+        assert!(cell_is_alive(&grid, 6, 6));
+
+        assert!(!cell_is_alive(&grid, 5, 5));
+        assert!(!cell_is_alive(&grid, 5, 7));
+
+        let grid = next_state_for_grid(&grid);
+        assert!(cell_is_alive(&grid, 5, 5));
+        assert!(cell_is_alive(&grid, 5, 6));
+        assert!(cell_is_alive(&grid, 5, 7));
+
+        assert!(!cell_is_alive(&grid, 4, 6));
+        assert!(!cell_is_alive(&grid, 6, 6));
+    }
 }
